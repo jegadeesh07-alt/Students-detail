a58543b67253bfe86e6998c4e77f5dd41c8ea4bb
jegadeesh07-alt <jegadeesh200421@gmail.com>
2025-04-26 23:02:54 +0530

    all UI are created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import SignupForm from "./components/SignupForm";
+import "./index.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="logo">
+      <img className="log-image" src="https://logodownload.org/wp-content/uploads/2014/04/amazon-logo-0.png" alt="can't reload " />
+     </div>
+    <SignupForm />
+
     </div>
   );
 }
